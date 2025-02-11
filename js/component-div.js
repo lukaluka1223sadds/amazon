e67c08b49f240ea74624552397b6f8d5d5bb4da8
lukaluka1223sadds <l85300192@gmail.com>
2025-02-11 03:44:35 +0100

    add product onmouseover size

--- js/component-div.js
+++ js/component-div.js
@@ -1,8 +1,8 @@
 function createComponent(start,end){
     for(let i=start; i<=end; i++){
-       document.write(`<div class="component-1-component-${i}"></div>`)
-       
+        document.write(`<div class="component-1-component-${i}"></div>`)
     }
 }
 
 createComponent(1,24)
+
